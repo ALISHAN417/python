1e77b6eb0cc99dc9c279a58265804149fc446e34
student alishan <alishanchaudahary417@gmail.com>
2026-07-09 16:21:27 +0530

    add firstprogram.py

--- firstprogram.py
+++ firstprogram.py
@@ -1,2 +1,45 @@
-print("Hello world") 
-print("rond")
+#oops
+"""class Student:
+    
+    def __init__(self,name,marks):
+        self.name= name
+        self.marks= marks
+        
+    def get_avg(self):
+        sum=0
+        for val in self.marks:
+            sum+=val
+            print("hi",self.name,"your core is ",sum/3)
+    
+s1 = Student("ali",[21,77,66])
+s1.get_avg()"""
+
+
+"""class Account:
+    def __init__(self, acc, bal):
+        self.account = acc
+        self.balance = bal
+
+    def debit(self, amount):
+        self.balance -= amount
+        print("Rs", amount, "was debited")
+        print("Total balance =", self.get_balance())
+
+    def credit(self, amount):
+        self.balance += amount
+        print("Rs", amount, "was credited")
+        print("Total balance =", self.get_balance())
+
+    def get_balance(self):
+        return self.balance
+
+
+acc1 = Account(10000, 12384)
+
+acc1.debit(1000)
+acc1.credit(200)
+acc1.credit(400)
+acc1.debit(230)"""
+        
+
+
